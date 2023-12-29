a2d122f6cb938dd1b3b2605c6a69ff0736e9ec5e
bugrakayacan <62428022+bugrakayacan@users.noreply.github.com>
2023-12-29 23:27:50 +0300

    Controllerlar eklendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,30 @@
-﻿using airlinesSys.Models;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using webprojectplanebooking.Models;
 
-namespace airlinesSys.Controllers
+namespace webprojectplanebooking.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
-            //Deneme
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
-
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
